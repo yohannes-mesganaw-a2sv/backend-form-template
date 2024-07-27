bd832defe3db73819387c7030817a8f5f6cd9cc2
yohannes-mesganaw-a2sv <142490379+yohannes-mesganaw-a2sv@users.noreply.github.com>
2024-07-27 11:26:06 +0300

    Update backend-assessment-starter-template.go
    template refinement

--- backend-assessment-starter-template.go
+++ backend-assessment-starter-template.go
@@ -23,17 +23,7 @@ var (
 
 // Handler to add a new book
 func addBookHandler(w http.ResponseWriter, r *http.Request) {
-    var book Book
-    // TODO: Decode the request body into the book struct
-    // TODO: Assign a unique ID to the book
-    // TODO: Add the book to the in-memory storage
-
-    // TODO: Return the added book as a response
-}
-
-// Handler to get the list of all books
-func getBooksHandler(w http.ResponseWriter, r *http.Request) {
-    // TODO: Encode and send the list of books as a response
+    // TODO: Implement the Function.
 }
 
 func main() {
